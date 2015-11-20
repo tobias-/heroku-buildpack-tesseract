d35a4e06fc99bbab41a8fe666e8a72cd0fa77648
Tobias Olsson <tobias.olsson@youcruit.com>
2015-11-20 15:40:42 +0100

    Compile and hopefully include tesseract

---
+++ bin/compile_tesseract.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -eux
+
+leptonica_version="1.72"
+tesseract_version="3.04.00"
+tesseract_languages=(swe eng dan)
+
+
+TESS_BUILD="$(mktemp -d)"
+echo "$TESS_BUILD"
+export CPPFLAGS="-I${BUILD_DIR}/include"
+export LDFLAGS="-L${BUILD_DIR}/lib"
+cd "$TESS_BUILD"
+if ! [ -f "${BUILD_DIR}/lib/liblept.a" ]; then
+(
+  curl http://www.leptonica.org/source/leptonica-${leptonica_version}.tar.gz | tar zx
+  cd leptonica-${leptonica_version}
+  ./configure --prefix="${BUILD_DIR}"
+  make
+  make install
+)
+fi
+
+if ! [ -f "${BUILD_DIR}/bin/tesseract" ]; then
+(
+  git clone --single-branch --branch ${tesseract_version} --depth 1 https://github.com/tesseract-ocr/tesseract tesseract
+  cd tesseract
+  echo "$TESS_BUILD"
+  export LIBLEPT_HEADERSDIR="${BUILD_DIR}/include"
+  ./configure --prefix="${BUILD_DIR}"
+  make
+  make install
+)
+fi
+
+(
+  cd "$BUILD_DIR"
+  for lang in "${tesseract_languages[@]}"; do
+    curl "https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.${lang}.tar.gz" | tar zx
+  done
+)
