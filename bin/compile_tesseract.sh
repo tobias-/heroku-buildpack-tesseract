54875db48d4d250b85a0a1bcac7206022ccbd7cb
Tobias Olsson <tobias.olsson@youcruit.com>
2015-11-23 14:41:09 +0100

    Try to use "cache"

--- bin/compile_tesseract.sh
+++ bin/compile_tesseract.sh
@@ -4,32 +4,33 @@ leptonica_version="1.72"
 tesseract_version="3.04.00"
 tesseract_languages=(swe eng dan)
 
-
-TESS_BUILD="$(mktemp -d)"
-echo "$TESS_BUILD"
-export CPPFLAGS="-I${BUILD_DIR}/include"
-export LDFLAGS="-L${BUILD_DIR}/lib"
-cd "$TESS_BUILD"
-if ! [ -f "${BUILD_DIR}/lib/liblept.a" ]; then
-(
-  curl http://www.leptonica.org/source/leptonica-${leptonica_version}.tar.gz | tar zx
-  cd leptonica-${leptonica_version}
-  ./configure --prefix="${BUILD_DIR}"
-  make
-  make install
-)
-fi
-
-if ! [ -f "${BUILD_DIR}/bin/tesseract" ]; then
-(
-  git clone --single-branch --branch ${tesseract_version} --depth 1 https://github.com/tesseract-ocr/tesseract tesseract
-  cd tesseract
+if  ! wget -O- "https://s3.amazonaws.com/youcruit-us-cache/tesseract/tesseract_${tessract_version}.tbz2" | tar jx -C "${BUILD_DIR}"
+  TESS_BUILD="$(mktemp -d)"
   echo "$TESS_BUILD"
-  export LIBLEPT_HEADERSDIR="${BUILD_DIR}/include"
-  ./configure --prefix="${BUILD_DIR}"
-  make
-  make install
-)
+  export CPPFLAGS="-I${BUILD_DIR}/include"
+  export LDFLAGS="-L${BUILD_DIR}/lib"
+  cd "$TESS_BUILD"
+  if ! [ -f "${BUILD_DIR}/lib/liblept.a" ]; then
+  (
+    curl http://www.leptonica.org/source/leptonica-${leptonica_version}.tar.gz | tar zx
+    cd leptonica-${leptonica_version}
+    ./configure --prefix="${BUILD_DIR}"
+    make
+    make install
+  )
+  fi
+
+  if ! [ -f "${BUILD_DIR}/bin/tesseract" ]; then
+  (
+    git clone --single-branch --branch ${tesseract_version} --depth 1 https://github.com/tesseract-ocr/tesseract tesseract
+    cd tesseract
+    echo "$TESS_BUILD"
+    export LIBLEPT_HEADERSDIR="${BUILD_DIR}/include"
+    ./configure --prefix="${BUILD_DIR}"
+    make
+    make install
+  )
+  fi
 fi
 
 (
@@ -38,3 +39,5 @@ fi
     curl "https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.${lang}.tar.gz" | tar zx
   done
 )
+
+
